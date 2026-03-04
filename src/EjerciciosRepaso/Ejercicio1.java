98e4ea2c46508d4d37c40033e1669c0f7ae2cfbd
Jose Roberto Leiva Salazar <jleivas@miumg.edu.gt>
2026-03-04 00:37:10 -0600

    Ejercicio1

--- src/EjerciciosRepaso/Ejercicio1.java
+++ src/EjerciciosRepaso/Ejercicio1.java
@@ -1,10 +1,21 @@
 package EjerciciosRepaso;
-
+import java.util.Scanner;
 public class Ejercicio1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner (System.in);
+		
+		System.out.print("Ingrese un numero: ");
+		int n = sc.nextInt();
+		
+		int suma=0;
+		
+		for (int i=1; i <=n; i++) {
+			if (i % 4 == 0) {
+				suma +=i;
+			}
+		}
+System.out.println("La suma de los numeros divisibles entre 4 es: " + suma);
 	}
 
 }
